4b84f9a1f228cef14b0b9541a0ea597fd18e8603
Dzmitry Paulavets <pekarman2@gmail.com>
2024-03-22 16:04:47 +0300

    Add project files.

---
+++ ExchangeFutures/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ExchangeFutures
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
